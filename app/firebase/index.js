8c1dc172abeebfa9895a8ad6dde899505dc07bb8
Batyr Kanzitdinov <kanzitdinov@gmail.com>
2017-04-24 20:45:34 -0400

    Update index.js

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -1,12 +1,12 @@
 const firebase = require('firebase');
 
 var config = {
-  apiKey: "AIzaSyAWTPqrI1ellnJb_ULFQDfiSPXKvjTySo8",
-  authDomain: "todoapp-5e01e.firebaseapp.com",
-  databaseURL: "https://todoapp-5e01e.firebaseio.com",
-  projectId: "todoapp-5e01e",
-  storageBucket: "todoapp-5e01e.appspot.com",
-  messagingSenderId: "349486486931"
+  apiKey            : "AIzaSyAWTPqrI1ellnJb_ULFQDfiSPXKvjTySo8",
+  authDomain        : "todoapp-5e01e.firebaseapp.com",
+  databaseURL       : "https://todoapp-5e01e.firebaseio.com",
+  projectId         : "todoapp-5e01e",
+  storageBucket     : "todoapp-5e01e.appspot.com",
+  messagingSenderId : "349486486931"
 };
 let firebaseApp = firebase.initializeApp(config);
 
